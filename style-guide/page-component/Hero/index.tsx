8a5660bf917ff359c5cb86d2221bc60231f0e887
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2023-01-02 22:57:32 +0530

    [feat] : redesign home page Hero Section

--- style-guide/page-component/Hero/index.tsx
+++ style-guide/page-component/Hero/index.tsx
@@ -17,42 +17,26 @@ const Hero = () => {
         <br /> Science <br /> Society
       </p>
       <div className={styles.dashboard}>
-        {/* <lottie-player
-          id="pattern"
-          src="https://assets1.lottiefiles.com/packages/lf20_WfrFg5.json"
-          background="transparent"
-          speed="0.4"
-          loop
-          autoplay
-        ></lottie-player> */}
         <div className={styles.hero_lottie}>
           <HeroLottie />
         </div>
-        <div className={styles.dividers}></div>
-        <div className={styles.dividers}>
-          <div
-            className={styles.dividers_first}
-            id="headTile"
-            style={{ zIndex: "16" }}
-          >
-            <div className={styles.title}>
-              <p>
-                Computer&nbsp;
-                <span className={styles.highlight} id="highlight">
-                  Science
-                </span>
-                <br />
-                Society
-              </p>
-            </div>
-            <div className={styles.subtitle} id="subtitle">
-              <p>Department of Computer Science and Engineering</p>
-              <p>National Institute of Technology, Silchar</p>
-            </div>
+        <div className={styles.bottomText}>
+          <p>CSS</p>
+        </div>
+        <div
+          className={styles.dividers_first}
+          id="headTile"
+          style={{ zIndex: "16" }}
+        >
+          <div className={styles.gradientbg}></div>
+          <div className={styles.title}>
+            COMPUTER <br /> SCIENCE <br /> SOCIETY
+          </div>
+          <div className={styles.subtitle} id="subtitle">
+            <p>Department of Computer Science and Engineering</p>
+            <p>National Institute of Technology, Silchar</p>
           </div>
         </div>
-        <div className={styles.dividers}></div>
-        <div className={styles.dividers}></div>
       </div>
     </div>
   );
