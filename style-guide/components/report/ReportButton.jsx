9638e651992d694ff916d571557b261fdee75409
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-01-15 19:30:05 +0530

    FIX: report button

--- style-guide/components/report/ReportButton.jsx
+++ style-guide/components/report/ReportButton.jsx
@@ -4,24 +4,9 @@ import Styles from "./styles.module.css";
 
 const ReportButton = (props) => {
   const {Report, setReport} = props
-    const [showButton, setShowButton] = useState(false);
-  
-    const handleScroll = () => {
-      if (window.scrollY > 300) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    };
-  
-    useEffect(() => {
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-  
   return (
     <div>
-        <div className={Styles.reportButton} style={showButton?{display: 'block'}:{display:'none'}} onClick={()=>{Report?setReport(false):setReport(true)}}>
+        <div className={Styles.reportButton} onClick={()=>{Report?setReport(false):setReport(true)}}>
           <img className={Styles.reportButton_img} src="/images/report.svg" alt="" />
         </div>
       {Report?<BugReport Report={Report} setReport={setReport} />:''}
