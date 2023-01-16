97ba0c33d45148b1f2ebabe3ffbaaa941af6d512
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2023-01-16 11:51:00 +0530

    update About Us"

--- style-guide/page-component/About-us/index.tsx
+++ style-guide/page-component/About-us/index.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import styles from "./style.module.css";
+import CloudLottie from "../../components/lottie-player/cloud";
+import ReactTyped from "react-typed";
+
 const AboutUs = () => {
   return (
     <div>
       <div className={styles.about}>
-        <div className={`${styles.dividers} ${styles.dividers_second} `}>
+        <div className={`${styles.dividers} ${styles.dividers_second}`}>
           <div className={styles.title}>
             <p>ABOUT US</p>
           </div>
-          <div className={styles.gradientbg}></div>
+          <ReactTyped
+            className={styles.typed}
+            strings={[
+              "TECHNICAL EVENTS",
+              "FUN EVENTS",
+              "SPEAKER SESSIONS",
+              "WORKSHOPS",
+            ]}
+            typeSpeed={100}
+            backSpeed={40}
+            loop
+          />
           <div className={styles.subtitle}>
             <p>
               The Computer Science Society, run by the CSE department of NIT
@@ -17,13 +31,11 @@ const AboutUs = () => {
             </p>
           </div>
         </div>
-        <div className={styles.dividers_third}>
-          <div className={styles.aboutcentre}>CONDUCT</div>
-          <br></br>
-          <span></span>
+        <div className={styles.dividers}>
+          <div id={styles.logo}>
+            <CloudLottie />
+          </div>
         </div>
-
-        <div className={styles.head_about}>ABOUT US</div>
       </div>
     </div>
   );
