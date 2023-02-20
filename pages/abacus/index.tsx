0137a05cb58db74b491076362b2efef9cd086eb2
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-02-20 12:47:42 +0530

    New Page: ENIGMA page added

--- pages/abacus/index.tsx
+++ pages/abacus/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Hero from '../../style-guide/components/Abacus-Hero-Section';
-import TimeStamp from '../../style-guide/components/Abacus-Timestamp-Section';
+import React from "react";
+import Hero from "../../style-guide/components/Abacus-Hero-Section";
+import TimeStamp from "../../style-guide/components/Abacus-Timestamp-Section";
 
 const Index = () => {
-    return (<>
-        <Hero />
-        <TimeStamp />
+  return (
+    <>
+      <Hero />
+      <TimeStamp />
     </>
-    )
-}
+  );
+};
 
 export default Index;
