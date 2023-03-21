6001c7592e8879d926535f447375a950fd9bd88c
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-03-21 18:47:32 +0530

    ADD: SignUp icon

--- style-guide/page-component/Layout/index.tsx
+++ style-guide/page-component/Layout/index.tsx
@@ -7,6 +7,7 @@ import GoTop from "../../components/go-top";
 import ToggleTheme from "../../components/toggle-theme";
 import Head from "next/head";
 import ReportButton from "../../components/report/ReportButton";
+import SignUpBtn from "../../components/SignUp/SignUp";
 
 interface LayoutProps {
   children: any;
@@ -15,6 +16,7 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   const [Report, setReport] = useState(false);
   const [load, setLoad] = useState(true);
+  const [signed, setSigned] = useState(false);
   useEffect(() => {
     loadercall();
   }, []);
@@ -33,6 +35,7 @@ const Layout = ({ children }: LayoutProps) => {
             <title>Computer Science Society</title>
           </Head>
           <GoTop />
+          <SignUpBtn signed={signed} setSigned={setSigned}/>
           <ToggleTheme>{children[0].props}</ToggleTheme>
           <Curosr />
           <ReportButton Report={Report} setReport={setReport} />
