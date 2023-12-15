dfdfab6305546b925ebfa79eb666d2102a7f04a3
Nawadeep Atreya <nawadeepatreyamsl@gmail.com>
2023-12-15 14:55:04 +0530

    Skeleton Effect in Gallery

--- style-guide/components/Esperanza/index.tsx
+++ style-guide/components/Esperanza/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import SkeletonElement from "./SkeletonElement";
 import styles from "./styles.module.css";
 
 const Esperanza = () => {
+  const [loading, setLoading] = useState(true);
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   const path1 = "./images/cultural/";
   const images1 = [
     ["1", "CSS Orientation"],
@@ -10,25 +14,48 @@ const Esperanza = () => {
     ["4", "CSS Orientation"],
     ["5", "CSS Orientation"],
   ];
+
   const path2 = "./images/esperanza/";
   const images2 = [1, 2, 3, 7, 4, 6, 8, 5, 9, 31];
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+    setLoading(false);
+  };
+
   return (
     <>
       <div className={styles.wrapper}>
         {images1.map((image, ind) => (
           <div key={ind} className={styles.item}>
-            <div className={styles.polaroid}>
-              <img src={path1 + image[0] + "-min.jpg"} alt="image" />
-              <div className={styles.caption}>{image[1]}</div>
-            </div>
+            {loading&& !imageLoaded? (
+              <SkeletonElement />
+            ) : (
+              <div className={styles.polaroid}>
+                <img
+                  src={path1 + image[0] + "-min.jpg"}
+                  alt="image"
+                  onLoad={handleImageLoad}
+                />
+                <div className={styles.caption}>{image[1]}</div>
+              </div>
+            )}
           </div>
         ))}
         {images2.map((image, ind) => (
           <div key={ind} className={styles.item}>
-            <div className={styles.polaroid}>
-              <img src={path2 + image + "-min.jpg"} alt="image" />
-              <div className={styles.caption}>Esperanza</div>
-            </div>
+            {loading&&imageLoaded ? (
+              <SkeletonElement />
+            ) : (
+              <div className={styles.polaroid}>
+                <img
+                  src={path2 + image + "-min.jpg"}
+                  alt="image"
+                  onLoad={handleImageLoad}
+                />
+                <div className={styles.caption}>Esperanza</div>
+              </div>
+            )}
           </div>
         ))}
       </div>
