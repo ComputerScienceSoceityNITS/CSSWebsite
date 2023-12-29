6ae2c204cd7a40536110ce8a6810a8ff321cd3d9
Swarup Chanda <swarupchanda1963@gmail.com>
2023-12-29 13:16:53 +0530

    Feat:Added the feature of new members page of 2024

--- style-guide/page-component/Members2024/index.tsx
+++ style-guide/page-component/Members2024/index.tsx
@@ -11,166 +11,166 @@ import PRWing from "../PrWingMembers";
 import Styles from "./members2024.module.css";
 
 interface MemberProps {
-    devWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    cpWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    executiveWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    coHead: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    mlWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    designWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    literaryWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    prWing: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-    members: {
-      avatar: { public_id: String; url: String };
-      socialMedia: [
-        { facebook: String; instagram: String; github: String; linkedin: String }
-      ];
-      _id: String;
-      name: String;
-      role: String;
-      session: String;
-      year: String;
-    }[];
-  }
-  
-  const index = ({
-    devWing,
-    cpWing,
-    executiveWing,
-    coHead,
-    mlWing,
-    designWing,
-    literaryWing,
-    prWing,
-    members,
-  }: MemberProps) => {
-    const devCoHeads = members.filter((head) => {
-      return head.role === "Dev-Wing Head";
-    });
-    const cpCoHeads = members.filter((head) => {
-      return head.role === "CP-Wing Head";
-    });
-    const seniorExe = members.filter((head) => {
-      return head.role === "Executive-Wing Head";
-    });
-    const mlCoHeads = members.filter((head) => {
-      return head.role === "ML-Wing Head";
-    });
-    const designCoHeads = members.filter((head) => {
-      return head.role === "Design-Wing Head";
-    });
-    const literatureCoHeads = members.filter((head) => {
-      return head.role === "Literary-Wing Head";
-    });
-    const prCoHeads = members.filter((head) => {
-      return head.role === "PR-Wing Head";
-    });
-    return (
-      <>
-        <section className={Styles.memsection}>
-          <div className={Styles.tabPanel} id={Styles.tabPanel}>
-            <div className={Styles.wrapper}>
-              <h1 className={Styles.memberTag}>Members 23-24</h1>
-              {/* <Fade bottom> */}
-              <SocietyHeads members={members} />
-              <DevWing members={devWing} coHeads={devCoHeads} />
-              <div className={Styles.gap}></div>
-              <CPWing members={cpWing} coHeads={cpCoHeads} />
-              <div className={Styles.gap}></div>
-              <ExeWing members={executiveWing} coHeads={seniorExe} />
-              <div className={Styles.gap}></div>
-              <PRWing members={prWing} coHeads={prCoHeads} />
-              <div className={Styles.gap}></div>
-              <MLWing members={mlWing} coHeads={mlCoHeads} />
-              <div className={Styles.gap}></div>
-              <DesignWing members={designWing} coHeads={designCoHeads} />
-              <div className={Styles.gap}></div>
-              <LiteraryWing members={literaryWing} coHeads={literatureCoHeads} />
-              {/* </Fade> */}
-            </div>
+  devWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  cpWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  executiveWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  coHead: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  mlWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  designWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  literaryWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  prWing: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+  members: {
+    avatar: { public_id: String; url: String };
+    socialMedia: [
+      { facebook: String; instagram: String; github: String; linkedin: String }
+    ];
+    _id: String;
+    name: String;
+    role: String;
+    session: String;
+    year: String;
+  }[];
+}
+
+const index = ({
+  devWing,
+  cpWing,
+  executiveWing,
+  coHead,
+  mlWing,
+  designWing,
+  literaryWing,
+  prWing,
+  members,
+}: MemberProps) => {
+  const devCoHeads = members.filter((head) => {
+    return head.role === "Dev-Wing Head";
+  });
+  const cpCoHeads = members.filter((head) => {
+    return head.role === "CP-Wing Head";
+  });
+  const seniorExe = members.filter((head) => {
+    return head.role === "Executive-Wing Head";
+  });
+  const mlCoHeads = members.filter((head) => {
+    return head.role === "ML-Wing Head";
+  });
+  const designCoHeads = members.filter((head) => {
+    return head.role === "Design-Wing Head";
+  });
+  const literatureCoHeads = members.filter((head) => {
+    return head.role === "Literary-Wing Head";
+  });
+  const prCoHeads = members.filter((head) => {
+    return head.role === "PR-Wing Head";
+  });
+  return (
+    <>
+      <section className={Styles.memsection}>
+        <div className={Styles.tabPanel} id={Styles.tabPanel}>
+          <div className={Styles.wrapper}>
+            <h1 className={Styles.memberTag}>Members 23-24</h1>
+            {/* <Fade bottom> */}
+            <SocietyHeads members={members} />
+            <DevWing members={devWing} coHeads={devCoHeads} />
+            <div className={Styles.gap}></div>
+            <CPWing members={cpWing} coHeads={cpCoHeads} />
+            <div className={Styles.gap}></div>
+            <ExeWing members={executiveWing} coHeads={seniorExe} />
+            <div className={Styles.gap}></div>
+            <PRWing members={prWing} coHeads={prCoHeads} />
+            <div className={Styles.gap}></div>
+            <MLWing members={mlWing} coHeads={mlCoHeads} />
+            <div className={Styles.gap}></div>
+            <DesignWing members={designWing} coHeads={designCoHeads} />
+            <div className={Styles.gap}></div>
+            <LiteraryWing members={literaryWing} coHeads={literatureCoHeads} />
+            {/* </Fade> */}
           </div>
-        </section>
-      </>
-    );
-  };
-  
-  export default index;
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default index;
