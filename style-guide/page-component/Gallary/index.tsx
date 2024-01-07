39775787679a570b188c6f48bdb3ce75f7bb5cb9
Its_Nongamba <ngangomnongamba3@gmail.com>
2024-01-07 21:12:41 +0530

    Further Changes In Gallery UI

--- style-guide/page-component/Gallary/index.tsx
+++ style-guide/page-component/Gallary/index.tsx
@@ -9,23 +9,27 @@ const Gallery = () => {
   return (
     <>
       <div className={styles.header}>
-        <div className={styles.lottiecont}>
+        <div className={styles.headerContainer}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <div className={styles.lottiecont}>
           {" "}
           <BrightGallery />
         </div>
-        
           <div className={styles.header_content}>
             <p className={styles.gallerytext}>Gallery</p>
             <span className={styles.gallerytext}>
               A Picture Is Worth A Thousand Words.
             </span>
           </div>
+        </div>
+        
       <div className={styles.buttonContainer}>
         <button
           onClick={() => setActive(true)}
           style={{
             color: active ? "rgb(228, 31, 111)" : "rgb(68, 68, 68)",
-            borderBottom: active ? "1px rgb(228, 31, 111) solid" : "none",
             boxShadow: active ? "0 0 20px rgb(228, 31, 111)" : "none",
           }}
         >
@@ -35,7 +39,6 @@ const Gallery = () => {
           onClick={() => setActive(false)}
           style={{
             color: !active ? "rgb(228, 31, 111)" : "rgb(68, 68, 68)",
-            borderBottom: !active ? "1px rgb(228, 31, 111) solid" : "none",
             boxShadow: !active ? "0 0 20px rgb(228, 31, 111)" : "none",
           }}
         >
